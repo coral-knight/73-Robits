d452634d22ab7a788e781918525083a3b39dcc16
Marcos Menezes <marcosnunes12340987@gmail.com>
2024-03-05 15:37:05 -0300

    Transformed map to a 2d array of lists + Changed name of functions + Fixed expand(), add_point() and to_png()

--- robot-code/main.py
+++ robot-code/main.py
@@ -11,6 +11,16 @@ def main():
     ''''''
     map = Map()
 
+    map.expand([0.13, 0.13])
+    map.expand([-0.13, -0.13])
+
+    map.add_point([0.03, 0.03])
+    map.add_point([-0.03, -0.03])
+    map.print_map()
+    map.map_png()
+
+
+
     #robot = Robot(time_step=16)
     #start = robot.hardware.getTime()
     #robot.run()
--- robot-code/mapping/map.py
+++ robot-code/mapping/map.py
@@ -11,35 +11,42 @@ class Map:
         self.range_y = [-self.resolution, self.resolution]
         
         # map[x, y]
-        self.map = np.zeros(self.size, dtype=int)
+        self.map = np.empty(self.size, dtype=object)
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                self.map[x, y] = [0]
 
 
     def expand(self, point):
         if point[0] < self.range_x[0]:
             dif_map_x = math.ceil((self.range_x[0] - point[0])/self.resolution)
             while dif_map_x:
-                self.map = np.insert(self.map, 0, 0, axis=0)
+                self.map = np.insert(self.map, 0, None, axis=0)
+                for y in range(np.size(self.map, 1)): self.map[0, y] = [0]
                 self.range_x[0] = self.range_x[0] - self.resolution
                 dif_map_x = dif_map_x - 1
 
         if point[0] > self.range_x[1]: 
             dif_map_x = math.ceil((point[0] - self.range_x[1])/self.resolution)
             while dif_map_x:
-                self.map = np.insert(self.map, np.size(self.map, 0)-1, 0, axis=0)
+                self.map = np.insert(self.map, np.size(self.map, 0), None, axis=0)
+                for y in range(np.size(self.map, 1)): self.map[np.size(self.map, 0)-1, y] = [0]
                 self.range_x[1] = self.range_x[1] + self.resolution
                 dif_map_x = dif_map_x - 1
 
         if point[1] < self.range_y[0]: 
             dif_map_y = math.ceil((self.range_y[0] - point[1])/self.resolution)
             while dif_map_y:
-                self.map = np.insert(self.map, 0, 0, axis=1)
+                self.map = np.insert(self.map, 0, None, axis=1)
+                for x in range(np.size(self.map, 0)): self.map[x, 0] = [0]
                 self.range_y[0] = self.range_y[0] - self.resolution
                 dif_map_y = dif_map_y - 1
 
         if point[1] > self.range_y[1]: 
             dif_map_y = math.ceil((point[1] - self.range_y[1])/self.resolution)
             while dif_map_y:
-                self.map = np.insert(self.map, np.size(self.map, 1)-1, 0, axis=1)
+                self.map = np.insert(self.map, np.size(self.map, 1), None, axis=1)
+                for x in range(np.size(self.map, 0)): self.map[x, np.size(self.map, 1)-1] = [0]
                 self.range_y[1] = self.range_y[1] + self.resolution
                 dif_map_y = dif_map_y - 1
 
@@ -58,15 +65,21 @@ class Map:
                 self.range_y[0]+map_point[1]*self.resolution+self.resolution/2]
 
 
-    def add_point(self, point, type):
+    def add_point(self, point):
         mapx, mapy = self.real_to_map(point)
-        print("added", type, "at", mapx, mapy)
-        self.map[mapx, mapy] = type
+        self.map[mapx, mapy].append(point)
 
         return
+    
+
+    def print(self):
+        for y in range(np.size(self.map, 1)):
+            for x in range(np.size(self.map, 0)):
+                print(self.map[x, y], end=" ")
+            print(" ")
 
 
-    def print_real_map(self):
+    def print_real(self):
         for y in range(np.size(self.map, 1)):
             for x in range(np.size(self.map, 0)):
                 print(self.map_to_real([x, y]), end=" ")
@@ -75,12 +88,13 @@ class Map:
         return
     
 
-    def map_png(self):
+    def to_png(self):
         transpose = np.zeros([np.size(self.map, 1), np.size(self.map, 0)])
 
         for y in range(np.size(self.map, 1)):
             for x in range(np.size(self.map, 0)):
-                transpose[y, x] = self.map[x, np.size(self.map, 1)-1-y]
+                if len(self.map[x, np.size(self.map, 1)-1-y]) > 1:
+                    transpose[y, x] = 1
 
         s = 'teste1.png'
         M = abs(transpose)*255
